b89bff5ceef2607ffcef518e0dfca904c00907d9
Henrique Miranda <miranda.henrique@gmail.com>
2020-03-31 00:51:13 +0200

    Added fit to logistic curve. Some countries like Austria start to show a deacelaration of the number of new cases

--- plot_covid19_daily.py
+++ plot_covid19_daily.py
@@ -13,6 +13,7 @@ from scipy.optimize import curve_fit
 AVG_GROWTH_RATE_POINTS = 3
 DATA_FILE_PATH = "COVID-19/csse_covid_19_data/csse_covid_19_daily_reports"
 START_DAYS = -20
+WHAT = 'Deaths'
 WHAT = 'Confirmed'
 
 def average_growth_rate(values) -> Tuple[int, int]:
@@ -56,28 +57,43 @@ def get_data_country(country):
         y.append( dat.at[country_row,WHAT] )
     return x,y
 
-def get_model(x, y):
+def get_model_logistic(x, y):
     # fit model
-    L = 10000
+    L = 100000
+    k = 0.3
+    x0 = -10
+    p0 = [k, x0, L]
+    lbound = [0,-200,1]
+    ubound = [1,0,5000000]
+    args, pcov = curve_fit(fit_logistic, x, y, p0, bounds=(lbound,ubound), maxfev=100000)
+
+    # compute model
+    x_prev = np.linspace(START_DAYS, 7)
+    y_prev = fit_logistic(x_prev, *args)
+
+    return x_prev, y_prev, args
+
+def get_model_exp(x, y):
+    # fit model
     k = 0.3
     x0 = -10
     p0 = [k, x0]
     lbound = [0,-200]
     ubound = [1,0]
-    args, pcov = curve_fit(fit, x, y, p0, bounds=(lbound,ubound), maxfev=100000)
+    args, pcov = curve_fit(fit_exp, x, y, p0, bounds=(lbound,ubound), maxfev=100000)
 
     # compute model
     x_prev = np.linspace(START_DAYS, 7)
-    y_prev = fit(x_prev, *args)
+    y_prev = fit_exp(x_prev, *args)
 
     return x_prev, y_prev, args
 
 
-def fit_log(x, k, x0, L):
+def fit_logistic(x, k, x0, L):
     return L / (1 + np.exp(-k * (x - x0)))
 
 
-def fit(x, k, x0):
+def fit_exp(x, k, x0):
     return np.exp(k * (x - x0))
 
 
@@ -103,6 +119,7 @@ if __name__ == "__main__":
         ("Italy", ""),
         # ("Vietnam", ""),
         #("Denmark", ""),
+        ("Belgium", ""),
         ("Spain", ""),
         ("Portugal", "")
     ]
@@ -111,16 +128,25 @@ if __name__ == "__main__":
     ax.set_title(WHAT)
     for i, country_province in enumerate(countries):
         x, y = get_data_country(country=country_province[0])
-        x_prev, y_prev, args = get_model(x, y)
+        line, = ax.plot(x, y, linewidth=1.5, marker=".")
         avg_units, avg_percentage = average_growth_rate(y[-AVG_GROWTH_RATE_POINTS:])
         print("Average growth rate in the last {} days in {} is {} ({:.0f}%)".format(AVG_GROWTH_RATE_POINTS,
                                                                                      country_province[0],
                                                                                      avg_units,
                                                                                      avg_percentage))
 
-        line, = ax.plot(x, y, linewidth=1.5, marker=".")
-        label = 'exp(%6.4lf*(x%+lf)) %s'%(args[0],args[1],country_province[0])
-        ax.plot(x_prev, y_prev, label=label, linewidth=1.5, dashes=[6, 2], color=line.get_color())
+
+        x_prev, y_prev, args = get_model_exp(x, y)
+        label_exp = 'exp(%6.4lf*(x%+lf)) %s'%(args[0],args[1],country_province[0])
+        ax.plot(x_prev, y_prev, label=label_exp, linewidth=1.5, dashes=[6, 2], color=line.get_color())
+
+        x_prev, y_prev, args = get_model_logistic(x, y)
+        label_log = '%8d / exp(%6.4lf*(x%+6.4lf)) %s'%(args[2],args[0],args[1],country_province[0])
+        label_log = None
+        ax.plot(x_prev, y_prev, label=label_log, linewidth=1.5, dashes=[6, 2], color=line.get_color())
+        ax.annotate("%s:%d"%(country_province[0],round(args[2],-3)), (x_prev[-1], args[2]),fontsize="x-small")
+        ax.axhline(args[2],lw=1,alpha=0.3,color=line.get_color())
+
         for i, txt in enumerate(y):
             if txt < 0: continue
             ax.annotate(txt, (x[i], y[i]),fontsize="x-small", rotation="90")
@@ -130,6 +156,7 @@ if __name__ == "__main__":
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         ax.grid(True)
 
+    plt.xlim(None,x_prev[-1]+5)
     plt.legend()
     plt.tight_layout()
     plt.show()
